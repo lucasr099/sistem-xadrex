f4697df37e07f0eb868b9e646fd67fbb79c1424c
Lucas Costa <lucasreiiss100@hotmail.com>
2025-10-30 18:15:33 -0300

    pequeno ajustes no tabuleiro

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,8 +1,11 @@
 package chess.pieces;
 
+
+
 import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
+
 
 public class King extends ChessPiece {
     public King(Board board, Color color) {
