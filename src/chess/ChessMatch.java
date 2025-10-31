0e4b9eeb0a6c5d390a6f6a2d4a598f49c1af1724
Lucas Costa <lucasreiiss100@hotmail.com>
2025-10-30 22:36:27 -0300

    Movendo as peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,8 +1,13 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
+
+import javax.xml.transform.Source;
+import java.util.Scanner;
 
 public class ChessMatch {
     private Board board;
@@ -20,6 +25,27 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validateSourcePosition(source);
+        Piece capturePiece = makeMove(source,target);
+        return (ChessPiece)capturePiece;
+
+    }
+    private Piece makeMove(Position source, Position target){
+        Piece p = board.removePiece(source);
+        Piece capturePiece = board.removePiece(target);
+        board.placePiece(p,target);
+        return capturePiece;
+
+    }
+    private void validateSourcePosition(Position position){
+        if (!board.thereIsAPiece(position)){
+            throw new ChessException("Não Há peça nessa posição");
+        }
+
     }
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column,row).toPosition());
