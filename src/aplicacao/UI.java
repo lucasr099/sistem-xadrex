7cabf6a8fcea9533af7af1eb62c46b80a94ddea9
Lucas Costa <lucasreiiss100@hotmail.com>
2025-11-04 19:42:49 -0300

    Chequemate

--- src/aplicacao/UI.java
+++ src/aplicacao/UI.java
@@ -55,9 +55,15 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turno : " + chessMatch.getTurn());
-        System.out.println("Waiting Player : " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()) {
-            System.out.println("CHECK!");
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Waiting Player : " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                System.out.println("CHECK!");
+            }
+        }
+        else {
+            System.out.println("CHECKMATE!");
+            System.out.println("Vencedor: " + chessMatch.getCurrentPlayer());
         }
     }
     public static void printBoard(ChessPiece[][] pieces) {
